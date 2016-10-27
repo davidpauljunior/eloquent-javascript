4f78a660e21744f03b2ea196884d9a6332e6c0ae
David Paul <davidpauljunior@gmail.com>
2016-10-27 22:52:57 +0100

    Adds bean counting exercise

--- src/js/eloquent.js
+++ src/js/eloquent.js
@@ -4,6 +4,7 @@ const fizzBuzz = require('./modules/02-program-structure/fizz-buzz');
 const chessBoard = require('./modules/02-program-structure/chess-board');
 const minimum = require('./modules/03-functions/minimum');
 const recursion = require('./modules/03-functions/recursion');
+const beanCounting = require('./modules/03-functions/bean-counting');
 
 document.addEventListener('readystatechange', function () {
     if (document.readyState === 'complete') {
@@ -20,6 +21,9 @@ document.addEventListener('readystatechange', function () {
         console.log(exerciseBreak);
 
         recursion.init();
+        console.log(exerciseBreak);
+
+        beanCounting.init();
         console.log(exerciseBreak);
     }
 });
