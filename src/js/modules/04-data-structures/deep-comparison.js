80e46a27528cfb03f9f8889e95732abb9b953cc6
David Paul <davidpauljunior@gmail.com>
2017-03-08 12:34:38 +0000

    Checks if child prop is an object and reassigns var

--- src/js/modules/04-data-structures/deep-comparison.js
+++ src/js/modules/04-data-structures/deep-comparison.js
@@ -7,66 +7,79 @@
 
 function deepEqual(obj, obj2) {
 
+    let objectMatch = false;
+
     // Early exit 1
     // Checks whether obj and obj2 are null or not 'object'
     if ((obj === null || typeof(obj) != 'object') && (obj2 === null || typeof(obj2) != 'object')) {
+        console.log('One of both of the arguments passed in to deepEqual were not objects, or were null.')
         return false;
     }
 
     // Early exit 2
     // Checks whether obj and obj2 have the same number of properties
     if (Object.keys(obj).length != Object.keys(obj2).length) {
-        console.log('different number of props');
+        console.log('The objects have a different number of properties');
         return false;
     }
 
     // Now need to loop through keys
-    
     for (prop in obj) {
 
         // Early exit 3
         // Checks that obj2 has same properties as obj
         // .hasOwnProperty returns a boolean
         if (!obj2.hasOwnProperty(prop)) {
+            console.log('Objects have different properties');
             return false;
         }
 
-        // console.log(obj[prop]); // returns string because the key is a string, e.g. 'hello', 'object'
-        // console.log(obj2[prop]);
+        // Need to check here if they are objects.
+        // Just because they're not objects, doesn't mean the keys don't match.
+        // So if the property of they keys (of obj 1 and 2) are an object, run deepEqual again.
+        if (typeof(obj[prop]) === 'object' && typeof(obj2[prop]) === 'object') {
+            deepEqual(obj[prop], obj2[prop]);
+        }
 
-        // Call deepEqual again and passthrough the looped through properies (actually gives values)
-        // If that returns false, return this loop false.
-        // console.log(obj2[prop]);
-        console.log(obj[prop]);
-        console.log(typeof(obj[prop]));
-        console.log(deepEqual(obj[prop], obj2[prop]));
-        // if (!deepEqual(obj[prop], obj2[prop])) {
-        //     // console.log(
-        //     //     typeof(obj[prop]), // returns string, need obj.
-        //     //     typeof(obj2[prop]) // returns undefined
-        //     // );
-        //     return false;
-        // }
+        // If the properties are not objects, check if they are the same.
+        if (obj[prop] === obj2[prop]) {
+            objectMatch = true;
+        } else {
+            console.log('properties didn\'t match');
+            objectMatch = false;
+        }
     }
-    return true;
+
+    return objectMatch;
 }
 
 function init() {
-    const myObject = {
+    const simpleObject = {
+        key: 1,
+        key: 2
+    };
+
+    const simpleObject2 = {
+        key1: 1,
+        key2: 2
+    };
+
+    const complexObject = {
         objFirst: {
             objFirstSub: 'obj-child-object'
         },
         objSecond: 2
     };
 
-    const myObject2 = {
+    const complexObject2 = {
         obj2First: 'obj2 string',
-        obj2Second: 3,
-        obj2Third: 4
+        obj2Second: 3
     };
 
-    console.log(deepEqual(myObject, myObject));
-    // console.log(deepEqual(myObject, myObject2));
+    console.log(deepEqual(simpleObject, simpleObject));
+    console.log(deepEqual(simpleObject, simpleObject2));
+    console.log(deepEqual(complexObject, complexObject));
+    console.log(deepEqual(complexObject, complexObject2));
 }
 
 module.exports = { init };
