4480a20250f275be113cfa14413d2a7c0242b312
David Paul <davidpauljunior@gmail.com>
2017-04-28 22:28:28 +0100

    Fixes syntax for todo in test

--- test/looping-triangle-test.js
+++ test/looping-triangle-test.js
@@ -1,8 +1,7 @@
 import test from 'ava';
 import loopingTriangle from '../src/js/modules/02-program-structure/looping-triangle';
 
-test.todo('expect a triangle of hashes to be returned', t => {
-    // Need to stub console.log and then check number of times
-    // it's called and what it's outputting.
-    // t.is(loopingTriangle.init(), '#######');
-});
+test.todo('expect a triangle of hashes to be returned');
+// Need to stub console.log and then check number of times
+// it's called and what it's outputting.
+// t.is(loopingTriangle.init(), '#######');
